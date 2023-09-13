5b00a06450473393af0c80d63189f5585533cb0f
nad <17@.ru>
2023-09-13 17:55:20 +0500

    dd

---
+++ apple.js
@@ -0,0 +1,11 @@
+var X = +prompt("Введите первое число");
+
+var Y = +prompt("Введите второе число");
+
+var Z = +prompt("Введите третье число");
+
+var finish = ((X + Y + Z));
+
+var fin = ((finish / 2));
+
+alert("Среднее арифметическое:" + fin)
